7bd3541e3ebbab349f80169d685cf9c7b2593076
seungjun-dev <seungjun91@naver.com>
2019-07-10 23:47:46 +0900

    apply bootstrap

--- component/password-check/check.js
+++ component/password-check/check.js
@@ -1,10 +1,12 @@
 function checkPw() {
-    let pwInput = document.getElementById('pwInput').innerHTML;
-    let pwCheck = document.getElementById('pwCheck').innerHTML;
+    let pwInput = document.getElementById('pwInput').value;
+    let pwCheck = document.getElementById('pwCheck').value;
     
     if(pwInput==pwCheck) {
-        alert('Password Confirmed');
+        //alert('Password Confirmed');
+        document.getElementById('confirm').innerText='비밀번호가 일치합니다.';
     } else {
-        alert('Please check your password');
+        //alert('Please check your password');
+        document.getElementById('confirm').innerText='비밀번호가 일치하지 않습니다.';
     }
 }
