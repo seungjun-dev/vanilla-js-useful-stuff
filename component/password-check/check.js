dd9722987153b6c6c895f82c95d89ef780f52fe9
seungjun-dev <seungjun91@naver.com>
2019-07-11 21:14:29 +0900

    change color of confirmation text

--- component/password-check/check.js
+++ component/password-check/check.js
@@ -1,12 +1,15 @@
 function checkPw() {
     let pwInput = document.getElementById('pwInput').value;
     let pwCheck = document.getElementById('pwCheck').value;
-    
+    let confirm = document.getElementById('confirm');    
+
     if(pwInput==pwCheck) {
         //alert('Password Confirmed');
-        document.getElementById('confirm').innerText='비밀번호가 일치합니다.';
+        confirm.innerText='비밀번호가 일치합니다.';
+        confirm.setAttribute('style','color:blue');
     } else {
         //alert('Please check your password');
-        document.getElementById('confirm').innerText='비밀번호가 일치하지 않습니다.';
+        confirm.innerText='비밀번호가 일치하지 않습니다.';
+        confirm.setAttribute('style','color:red');
     }
 }
